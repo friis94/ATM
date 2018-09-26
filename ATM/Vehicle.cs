cbf374608302b47deb4c1aa892eca85e3b04e3e3
friis94 <nanna_friis@live.dk>
2018-09-26 11:40:48 +0200

    Adding Vehicle class

---
+++ ATM/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Vehicle :IVehicle
+    {
+        public string Tag { get; set; }
+        public int Xcoordinate { get; set; }
+        public int Ycoordinate { get; set; }
+        public int Altitude { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
