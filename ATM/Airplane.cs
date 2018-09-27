3a6db02c5001adbd418d63032bc27ede57c0bcb1
Jakob Halskov <jakobhalskov@gmail.com>
2018-09-27 09:08:36 +0200

    Removing Vehicle.cs and adds velocity+course to IVehicle interface + Airplane class

--- ATM/Airplane.cs
+++ ATM/Airplane.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-    class Airplane
+
+    public class Airplane : IVehicle
     {
+        public string Tag { get; set; }
+        public int Xcoordinate { get; set; }
+        public int Ycoordinate { get; set; }
+        public int Altitude { get; set; }
+        public int velocity { get; set; }
+        public int course { get; set; }
+        public DateTime Timestamp { get; set; }
     }
 }
