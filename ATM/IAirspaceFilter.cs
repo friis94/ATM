4d5112bab3c65a910c0de0647e116685440124f5
Jakob Halskov <jakobhalskov@gmail.com>
2018-09-28 11:43:19 +0200

    Adding AirspaceFilter interface

---
+++ ATM/IAirspaceFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    interface IAirspaceFilter
+    {
+        void SetAirSpace(int xmin, int xmax, int ymin, int ymax, int altitudemin, int altitudemax);
+        List<IVehicle> FilterVehicles(List<IVehicle> vehicles);
+
+    }
+}
