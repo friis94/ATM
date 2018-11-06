6698cc2a145f24ce3b12bc6509735153eae59bf7
jakobww <jakobwilbrandt@gmail.com>
2018-11-06 10:11:48 +0100

    adding timer functionality and trackdetector class and interface

---
+++ ATM/ITimer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public interface ITimer
+    {
+        event EventHandler Expired;
+
+        void Start();
+        void Stop();
+    }
+}
---
+++ ATM/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+
+    public class Timer : ITimer
+    {
+        public int TimeRemaining { get; private set; }
+
+        public event EventHandler Expired;
+
+        private System.Timers.Timer timer;
+
+        public Timer(int time)
+        {
+            timer = new System.Timers.Timer();
+            // Bind OnTimerEvent with an object of this, and set up the event
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimerEvent);
+            timer.Interval = time;
+            timer.AutoReset = false;
+        }
+
+
+        public void Start()
+        {
+            timer.Enabled = true;
+        }
+
+        public void Stop()
+        {
+            timer.Enabled = false;
+        }
+
+        private void Expire()
+        {
+            timer.Enabled = false;
+            Expired?.Invoke(this, System.EventArgs.Empty);
+        }
+
+        private void OnTimerEvent(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            Expire();
+        }
+
+    }
+}
